c2deae502e3d3e73bc805bc503b99aa01a03d965
Yamaguchi <17.punch@gmail.com>
2017-04-19 01:25:23 +0900

    chacked ead key

--- famibasic_key/famibasic_key.ino
+++ famibasic_key/famibasic_key.ino
@@ -8,35 +8,39 @@ const int d_1 = 2;
 const int d_2 = 3;
 const int d_3 = 4;
 const int d_4 = 5;
-const int out_0 = 6;
-const int out_1 = 5;
-const int out_2 = 4;
+const int out_0 = 9;
+const int out_1 = 10;
+const int out_2 = 11;
 const int KEY_SIZE = 72;
-char key[KEY_SIZE];
+int key[KEY_SIZE];
 
-void ResetKey(char* key){
+void ResetKey(int* key){
   for(int i =0; i<KEY_SIZE; i++){
     key[i] = 0;
   }
 }
 
-void ReadKey(char* key){
+void ReadKey(int* key){
   //all key = 0
   ResetKey(key);
   digitalWrite(out_1, LOW);  
   digitalWrite(out_0, HIGH);  
-
+  delayMicroseconds(24d);
+  digitalWrite(out_0, LOW); 
   //read keys
   for(int i=0; i<9; i++){
     digitalWrite(out_1, LOW);  
-    digitalWrite(out_0, LOW);  
+    delayMicroseconds(24);
+ 
     if(digitalRead(d_1) == HIGH) key[i*8+0] = 1;
     if(digitalRead(d_2) == HIGH) key[i*8+1] = 1;
     if(digitalRead(d_3) == HIGH) key[i*8+2] = 1;
     if(digitalRead(d_4) == HIGH) key[i*8+3] = 1;
 
     digitalWrite(out_1, HIGH);  
-    digitalWrite(out_0, LOW);
+  
+    delayMicroseconds(24);
+
     if(digitalRead(d_1) == HIGH) key[i*8+4] = 1;
     if(digitalRead(d_2) == HIGH) key[i*8+5] = 1;
     if(digitalRead(d_3) == HIGH) key[i*8+6] = 1;
@@ -46,14 +50,10 @@ void ReadKey(char* key){
 }
 
 void setup() { // initialize the buttons' inputs:
-  pinMode(d_1, INPUT);
-  digitalWrite(d_1, LOW);  
+  pinMode(d_1, INPUT);  
   pinMode(d_2, INPUT);
-  digitalWrite(d_2, LOW);  
   pinMode(d_3, INPUT);
-  digitalWrite(d_3, LOW);  
-  pinMode(d_4, INPUT);
-  digitalWrite(d_4, LOW);   
+  pinMode(d_4, INPUT); 
   
   pinMode(out_0, OUTPUT);
   pinMode(out_1, OUTPUT);
@@ -66,7 +66,10 @@ void setup() { // initialize the buttons' inputs:
 
 void loop() {
   ReadKey(key);
-  Serial.println(key);
+  for(int i=0; i<KEY_SIZE; i++){
+    Serial.print(key[i]);
+  }
+  Serial.print('\n');
 }
 
 
