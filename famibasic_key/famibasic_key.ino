3e094cbdeb2514b481cd01a86ffdd3086a016afa
Yamaguchi <17.punch@gmail.com>
2017-04-23 20:41:15 +0900

    add SendKey() for work PC usb keyboard function

--- famibasic_key/famibasic_key.ino
+++ famibasic_key/famibasic_key.ino
@@ -3,48 +3,141 @@
 
  */
 
+#include <Keyboard.h>
+
 // set pin numbers for the five buttons:
 const int d_1 = 2;
 const int d_2 = 3;
 const int d_3 = 4;
 const int d_4 = 5;
-const int out_0 = 9;
-const int out_1 = 10;
-const int out_2 = 11;
+const int out_0 = 7;
+const int out_1 = 8;
+const int out_2 = 9;
 const int KEY_SIZE = 72;
-int key[KEY_SIZE];
+const int U_DELAY = 50;
+bool key[KEY_SIZE];
+bool key_pre[KEY_SIZE];
+const char KEY[KEY_SIZE]={
+  '1',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a', //10
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a', //10
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a', //10
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a', //10
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a', //10
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a', //10
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a',
+  'a', //10
+  'a',
+  'a'
+};
 
-void ResetKey(int* key){
+
+void ResetKey(bool* _key){
   for(int i =0; i<KEY_SIZE; i++){
-    key[i] = 0;
+    _key[i] = 0;
   }
 }
 
-void ReadKey(int* key){
+void SendKey(bool* _key, bool* _key_pre){
+  for(int i =0; i<KEY_SIZE; i++){
+    if(_key_pre[i]==0 && _key[i]==1 ){
+      Keyboard.press(KEY[i]);
+      _key_pre[i] = 1;
+      //Serial.print("press A \n");
+    }else if(_key_pre[i]==1 && _key[i]==0 ){
+      Keyboard.release(KEY[i]);
+      _key_pre[i] = 0;  
+      //Serial.print("release A \n");
+    }
+  }
+}
+
+void ReadKey(bool* _key){
   //all key = 0
-  ResetKey(key);
+  ResetKey(_key);
   digitalWrite(out_1, LOW);  
   digitalWrite(out_0, HIGH);  
-  delayMicroseconds(24d);
+  delayMicroseconds(U_DELAY);
   digitalWrite(out_0, LOW); 
   //read keys
   for(int i=0; i<9; i++){
     digitalWrite(out_1, LOW);  
-    delayMicroseconds(24);
+    delayMicroseconds(U_DELAY);
  
-    if(digitalRead(d_1) == HIGH) key[i*8+0] = 1;
-    if(digitalRead(d_2) == HIGH) key[i*8+1] = 1;
-    if(digitalRead(d_3) == HIGH) key[i*8+2] = 1;
-    if(digitalRead(d_4) == HIGH) key[i*8+3] = 1;
+    if(digitalRead(d_1) == HIGH) _key[i*8+0] = 1;
+    if(digitalRead(d_2) == HIGH) _key[i*8+1] = 1;
+    if(digitalRead(d_3) == HIGH) _key[i*8+2] = 1;
+    if(digitalRead(d_4) == HIGH) _key[i*8+3] = 1;
 
     digitalWrite(out_1, HIGH);  
   
-    delayMicroseconds(24);
+    delayMicroseconds(U_DELAY);
 
-    if(digitalRead(d_1) == HIGH) key[i*8+4] = 1;
-    if(digitalRead(d_2) == HIGH) key[i*8+5] = 1;
-    if(digitalRead(d_3) == HIGH) key[i*8+6] = 1;
-    if(digitalRead(d_4) == HIGH) key[i*8+7] = 1;
+    if(digitalRead(d_1) == HIGH) _key[i*8+4] = 1;
+    if(digitalRead(d_2) == HIGH) _key[i*8+5] = 1;
+    if(digitalRead(d_3) == HIGH) _key[i*8+6] = 1;
+    if(digitalRead(d_4) == HIGH) _key[i*8+7] = 1;
   }
   return;
 }
@@ -61,16 +154,21 @@ void setup() { // initialize the buttons' inputs:
 
   //set key bord
   digitalWrite(out_2, HIGH);
+  Keyboard.begin();
   Serial.begin(9600);
+
+  ResetKey(key);
+  ResetKey(key_pre);
 }
 
 void loop() {
   ReadKey(key);
+  SendKey(key, key_pre);
+ /* for debag sirial monitor
   for(int i=0; i<KEY_SIZE; i++){
     Serial.print(key[i]);
   }
   Serial.print('\n');
+*/
 }
-
-
 
